affa358c063e467704093f61e956c357fc9acdd4
Junghongki <honggi6516@naver.com>
2019-05-19 01:02:23 +0900

    Drawer Implement

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
+        canvasColor: Colors.transparent
       ),
       home: new MyHomePage(title: 'Flutter Bottom sheet'),
     );
@@ -55,15 +56,143 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+      drawer: Drawer(
+        child: Container(
+          width: 256,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+                bottomRight: const Radius.circular(16.0),
+                topRight: const Radius.circular(16.0)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              // 사용자 닉네임
+              Container(
+                padding: const EdgeInsets.only(top: 104.0, left: 24.0, right: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                      topRight: const Radius.circular(16.0)
+                  ),
+                ),
+                width: 256,
+                child: Text(
+                  'Nickname',
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+              // 사용자 집주소
+              Container(
+                padding: const EdgeInsets.only(
+                  top: 8.0, bottom: 16.0,
+                  left: 24.0, right: 16.0,
+                ),
+                width: 256,
+                color: Colors.amber,
+                child: Text(
+                  'address',
+                  style: TextStyle(fontSize: 15.0),
+                ),
+              ),
+              // 내가 도전한 퀘스트
+              Container(
+                padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                child: RaisedButton(
+                  elevation: 0.0,
+                  highlightElevation: 0.0,
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.amberAccent.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        child: Icon(Icons.local_airport),
+                      ),
+                      Text('내가 수행한 퀘스트')
+                    ],
+                  ),
+                  onPressed: () => {
+                    Navigator.pop(context)
+                  },
+                  color: Colors.transparent,
+                ),
+              ),
+              // 환경설정
+              Container(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: RaisedButton(
+                  elevation: 0.0,
+                  highlightElevation: 0.0,
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.amberAccent.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        child: Icon(Icons.settings),
+                      ),
+                      Text('환경설정')
+                    ],
+                  ),
+                  onPressed: () => {
+                    Navigator.pop(context)
+                  },
+                  color: Colors.transparent,
+                ),
+              ),
+            ],
+          ),
+        )
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: (){
-          _settingModalBottomSheet(context);
-        },
-        child: new Icon(Icons.add),
+      body: WillPopScope(
+          onWillPop: _yon,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent
+                  )
+              ),
+              Container(
+                margin: EdgeInsets.all(16.0),
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton(
+                    onPressed: () => {
+                      _settingModalBottomSheet(context)
+                    },
+                    child: Icon(Icons.add)
+                ),
+              ),
+            ],
+          )
       ),
+    );
+  }
+
+  Future<bool> _yon(){
+    return showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+            title: Text("알림"),
+            content: Text('정말 종료하시겠습니까?'),
+            actions: <Widget>[
+              FlatButton(
+                  child: Text("네"),
+                  onPressed: () => Navigator.pop(context, true)
+              ),
+              FlatButton(
+                  child: Text("아니요"),
+                  onPressed: () => Navigator.pop(context, false)
+              )
+            ]
+        )
     );
   }
 }
@@ -72,11 +201,11 @@ class _MyHomePageState extends State<MyHomePage> {
 void _settingModalBottomSheet(context){
   showModalBottomSheet(
       context: context,
-      builder: (WidgetBuilder) => bottomSheetBuilder()
+      builder: (WidgetBuilder) => bottomSheetBuilder(context)
   );
 }
 
-Widget bottomSheetBuilder(){
+Widget bottomSheetBuilder(BuildContext context){
   return Container(
     decoration: BoxDecoration(
       color: Colors.amber,
@@ -142,7 +271,9 @@ Widget bottomSheetBuilder(){
               child: RaisedButton(
                 padding: const EdgeInsets.only(top: 8, bottom: 8),
                 shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(40.0)),
-                onPressed: () => {},
+                onPressed: () => {
+                  Navigator.pop(context)
+                },
                 color: const Color(0xFFFFFFFF),
                 child: Icon(
                   Icons.insert_chart,
